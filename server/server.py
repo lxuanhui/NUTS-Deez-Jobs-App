24b875820d5247e3e079778ab8ba8d8667c6f2eb
trakugane <56292786+trakugane@users.noreply.github.com>
2023-02-25 20:54:14 +0800

    Added 1 more data into dataset

--- server/server.py
+++ server/server.py
@@ -23,13 +23,25 @@ def getTime():
         print("POST")
 
     return {
-     "logo": "https://media.licdn.com/dms/image/C560BAQE0iX_dgdH7nA/company-logo_100_100/0/1672279162274?e=1685577600&v=beta&t=0FlikIFsw3XQOIKrAu7lVW2yMtsGC-ZQFuyiBTLJO8g",
-     "companyName": "Shopee",
-     "salary": ["$5000", "LinkedIn"],
-     "yearOfExperience": [0, 2],
-     "noOfApplicants": [127, "LinkedIn"],
-     "typeOfWork": "WFH",
-     "source": ["LinkedIn", "https://www.linkedin.com/jobs/search/?currentJobId=3289369580&keywords=software%20engineer%20shopee"]
+        {
+             "logo": "https://media.licdn.com/dms/image/C560BAQE0iX_dgdH7nA/company-logo_100_100/0/1672279162274?e=1685577600&v=beta&t=0FlikIFsw3XQOIKrAu7lVW2yMtsGC-ZQFuyiBTLJO8g",
+             "companyName": "Shopee",
+             "salary": ["$5000", "LinkedIn"],
+             "yearOfExperience": [0, 2],
+             "noOfApplicants": [127, "LinkedIn"],
+             "typeOfWork": "WFH",
+             "source": ["LinkedIn", "https://www.linkedin.com/jobs/search/?currentJobId=3289369580&keywords=software%20engineer%20shopee"]
+        },
+        {
+            "logo": "https://media.licdn.com/dms/image/C560BAQE0iX_dgdH7nA/company-logo_100_100/0/1672279162274?e=1685577600&v=beta&t=0FlikIFsw3XQOIKrAu7lVW2yMtsGC-ZQFuyiBTLJO8g",
+            "companyName": "ByteDance",
+            "salary": ["$7000", "LinkedIn"],
+            "yearOfExperience": [0, 2],
+            "noOfApplicants": [127, "LinkedIn"],
+            "typeOfWork": "Office",
+            "source": ["LinkedIn",
+                       "https://www.linkedin.com/jobs/search/?currentJobId=3289369580&keywords=software%20engineer%20shopee"]
+        }
 
     }
 
