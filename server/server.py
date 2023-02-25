2e105ac9c02a0a5825afa002d7124d25e2974f53
trakugane <56292786+trakugane@users.noreply.github.com>
2023-02-26 02:55:17 +0800

    (Back-End) Able to retrieve and push 38 records from JobStreet and GrabJob to Front-End

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
+from JobScraper_2 import grabJobScraper
+from JobScraper import jobStreetScraper
 # import datetime
 # import requests
 # import bs4
@@ -16,34 +18,43 @@ def getTime():
     # data = request.data()
     # print(data)
 
+    grabJobData = grabJobScraper("software engineer")
+    jobStreetData = jobStreetScraper("software engineer")
+    print(grabJobData)
+    print(jobStreetData)
+
+    newData = grabJobData + jobStreetData
+
     if request.method == 'GET':
         print('GET')
 
     if request.method == 'POST':
         print("POST")
 
-    return {
-        {
-             "logo": "https://media.licdn.com/dms/image/C560BAQE0iX_dgdH7nA/company-logo_100_100/0/1672279162274?e=1685577600&v=beta&t=0FlikIFsw3XQOIKrAu7lVW2yMtsGC-ZQFuyiBTLJO8g",
-             "companyName": "Shopee",
-             "salary": ["$5000", "LinkedIn"],
-             "yearOfExperience": [0, 2],
-             "noOfApplicants": [127, "LinkedIn"],
-             "typeOfWork": "WFH",
-             "source": ["LinkedIn", "https://www.linkedin.com/jobs/search/?currentJobId=3289369580&keywords=software%20engineer%20shopee"]
-        },
-        {
-            "logo": "https://media.licdn.com/dms/image/C560BAQE0iX_dgdH7nA/company-logo_100_100/0/1672279162274?e=1685577600&v=beta&t=0FlikIFsw3XQOIKrAu7lVW2yMtsGC-ZQFuyiBTLJO8g",
-            "companyName": "ByteDance",
-            "salary": ["$7000", "LinkedIn"],
-            "yearOfExperience": [0, 2],
-            "noOfApplicants": [127, "LinkedIn"],
-            "typeOfWork": "Office",
-            "source": ["LinkedIn",
-                       "https://www.linkedin.com/jobs/search/?currentJobId=3289369580&keywords=software%20engineer%20shopee"]
-        }
+    return newData
+        # {
 
-    }
+        # {
+        #      "logo": "https://media.licdn.com/dms/image/C560BAQE0iX_dgdH7nA/company-logo_100_100/0/1672279162274?e=1685577600&v=beta&t=0FlikIFsw3XQOIKrAu7lVW2yMtsGC-ZQFuyiBTLJO8g",
+        #      "companyName": "Shopee",
+        #      "salary": ["$5000", "LinkedIn"],
+        #      "yearOfExperience": [0, 2],
+        #      "noOfApplicants": [127, "LinkedIn"],
+        #      "typeOfWork": "WFH",
+        #      "source": ["LinkedIn", "https://www.linkedin.com/jobs/search/?currentJobId=3289369580&keywords=software%20engineer%20shopee"]
+        # },
+        # {
+        #     "logo": "https://media.licdn.com/dms/image/C560BAQE0iX_dgdH7nA/company-logo_100_100/0/1672279162274?e=1685577600&v=beta&t=0FlikIFsw3XQOIKrAu7lVW2yMtsGC-ZQFuyiBTLJO8g",
+        #     "companyName": "ByteDance",
+        #     "salary": ["$7000", "LinkedIn"],
+        #     "yearOfExperience": [0, 2],
+        #     "noOfApplicants": [127, "LinkedIn"],
+        #     "typeOfWork": "Office",
+        #     "source": ["LinkedIn",
+        #                "https://www.linkedin.com/jobs/search/?currentJobId=3289369580&keywords=software%20engineer%20shopee"]
+        # }
+
+    # }
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
