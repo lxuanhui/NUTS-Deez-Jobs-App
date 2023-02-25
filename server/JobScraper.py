a860e92b0fcbf155956150b14c6c77570e82ef30
shxnying <gohshanying@yahoo.com.sg>
2023-02-26 00:58:31 +0800

    Dict Created

--- server/JobScraper.py
+++ server/JobScraper.py
@@ -6,6 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
+job_link_dict=[];
+job_title_dict=[]
+company_logo_dict=[]
+company_name_dict=[]
+sal=[]
+yearsOfExpDict = {}
 
 
 op = webdriver.ChromeOptions()
@@ -27,32 +33,31 @@ wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="jobList"]/div[2
 
 # Find all elements with data-automation="job-card-logo"
 job_elements = driver.find_elements(By.CSS_SELECTOR, '.sx2jih0.zcydq84u.es8sxo0.es8sxo3.es8sxo21.es8sxoi')
-job_link_dict=[];
 # Loop through each logo element and extract information
 for job in job_elements:
-    
     # Extract all information
     job_title = job.find_element(By.CLASS_NAME, "_1hr6tkx5._1hr6tkx7._1hr6tkxa.sx2jih0.sx2jihf.zcydq8h")#job_title.text
     job_link = job.find_element(By.CSS_SELECTOR, 'a._1hr6tkx5._1hr6tkx7._1hr6tkxa.sx2jih0.sx2jihf.zcydq8h')
     job_link_href = job_link.get_attribute('href')
     job_link_dict.append(job_link_href)
+    job_title_dict.append(job_title.text)
     
 
 # Find all elements with class "sx2jih0 rqoqz6" -- Company Logo
 images = driver.find_elements(By.CSS_SELECTOR, 'img.sx2jih0.rqoqz6')
 for image in images:
     src = image.get_attribute('src')
+    company_logo_dict.append(src)
 
 # Find all elements with class "sx2jih0 rqoqz6" -- Company Name
 company_names = driver.find_elements(By.CSS_SELECTOR,'a[data-automation="jobCardCompanyLink"].sx2jih0.sx2jihf.rqoqz4')
-
 for name in company_names:
-    comapny_name = name.text.strip()
+    company_name = name.text.strip()
+    company_name_dict.append(company_name)
 
 # Scrape for Salary
 target_elements = driver.find_elements(By.CSS_SELECTOR,".sx2jih0 .sx2jih0.zcydq84u.es8sxo0.es8sxo3.es8sxo21.es8sxoh")
 salary = ''
-sal=[]
 for element in target_elements:
     sal.append(element.text)
 for i in range(len(sal)):
@@ -61,7 +66,6 @@ for i in range(len(sal)):
 
 driver.close()
 
-yearsOfExpDict = {}
 with webdriver.Chrome(options=op, executable_path="/usr/local/bin/chromedriver") as driver:
     for i, link in enumerate(job_link_dict):
         actions = ActionChains(driver)
@@ -72,4 +76,24 @@ with webdriver.Chrome(options=op, executable_path="/usr/local/bin/chromedriver")
         yearsOfExpList = [elem.text for elem in yearsOfExpElements if 'year' in elem.text]
         yearsOfExpDict[i] = yearsOfExpList if yearsOfExpList else []
 
-print(yearsOfExpDict)
+
+searchQueryDict = []
+# Loop through each logo element and extract information
+
+print(len(company_name_dict))
+for i in range(len(company_logo_dict)):
+    tempDict = {}
+    tempDict['logo'] = company_logo_dict[i]
+    tempDict['companyName'] = company_name_dict[i]
+    tempDict['salary'] = [sal[i],"JobStreet"] if i < len(sal) else ["","JobStreet"]
+    tempDict['yearOfExperience'] = yearsOfExpDict[i] if i in yearsOfExpDict else []
+    tempDict['noOfApplicants'] = None
+    tempDict['typeOfWork'] = None
+    tempDict['source'] = ["JobStreet", job_link_dict[i]]
+
+    searchQueryDict.append(tempDict)
+
+
+print(searchQueryDict)
+
+
